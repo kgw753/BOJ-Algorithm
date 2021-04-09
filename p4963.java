487696bb1a17918c22144c386b8bc112db9c9189
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-04-09 18:41:18 +0900

    solved

--- p4963.java
+++ p4963.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Stream;
 
 public class p4963{
@@ -11,7 +13,6 @@ public class p4963{
     public static int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
     public static void dfs(int y, int x, int count){
         cnt[y][x] = count;
-
         for(int i = 0; i < dx.length; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
@@ -21,6 +22,28 @@ public class p4963{
                 }
             }
         }
+    }
+
+    public static Queue<int []> q = new LinkedList<>();
+    public static void bfs(int y, int x, int count){
+        q.add(new int[]{y, x});
+        cnt[y][x] = count;
+        while(!q.isEmpty()){
+            int[] now = q.remove();
+            y = now[0];
+            x = now[1];
+
+            for(int i = 0; i < dx.length; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && nx < w && ny >= 0 && ny < h){
+                    if(map[ny][nx] == 1 && cnt[ny][nx] == 0){
+                        bfs(ny, nx, count);
+                    }
+                }
+            }
+        }
+
     }
     public static void main(String[] args)throws Exception{
         ArrayList<Integer> ans = new ArrayList<>();
@@ -46,7 +69,7 @@ public class p4963{
             for(int i = 0; i < h; i++){
                 for(int j = 0; j < w; j++){
                     if(cnt[i][j] == 0 && map[i][j] == 1){
-                        dfs(i, j, ++count);
+                        bfs(i, j, ++count);
                     }
                 }
             }
