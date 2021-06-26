9f20b715d284f105846f6feb8e91b96690595e08
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-06-26 21:12:27 +0900

    solved

--- p2262.cpp
+++ p2262.cpp
@@ -1,36 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<int> V;
-void print(vector<int> v){
-    for(int i = 0;i < v.size(); i++){
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-}
 int main(){
-    cin >> N;
-    for(int i = 0; i < N; i++){
-        int input;
-        cin >> input;
-        V.push_back(input);
+    int n;
+
+    cin >> n;
+    vector<int> v(n, 0);
+    for(int i = 0; i < n ; i++){
+        cin >> v[i];
     }
 
     int answer = 0;
-    for(int i = 0; i < N - 1; i++){
-        int gap = 257;
-        int fail = 0;
-        for(int j = 1; j < V.size(); j++){
-            if(gap > abs(V[j] - V[j - 1])){
-                gap = abs(V[j] - V[j - 1]);
-                fail = max(V[j], V[j - 1]);
-            }
-        }
-        
-        V.erase(find(V.begin(), V.end(), fail));
-        answer += gap;
-    }
+    for(int i = n; i > 1; i--){
+        int idx = find(v.begin(), v.end(), i) - v.begin();
 
+        int fail;
+        if(idx + 1 >= v.size()){
+            fail = v[idx - 1];
+        }    
+        else if(idx - 1 < 0){
+            fail = v[idx + 1];
+        }
+        else{
+            fail = max(v[idx + 1], v[idx - 1]);
+        }
+
+        answer += abs(i - fail);
+        v.erase(find(v.begin(), v.end(), i));
+    }
     cout << answer << "\n";
 }
