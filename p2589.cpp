78bd19d38e3f5223fd9ff89b77890d95547f076c
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-07-18 13:07:32 +0900

    p2589

--- p2589.cpp
+++ p2589.cpp
@@ -1,71 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 50
-int N, M;
+int N, M, ans = -1;
 string input;
-vector<vector<char>> MAP(MAX, vector<char>(MAX));
+vector<vector<int>> MAP(MAX, vector<int>(MAX, 0));
 vector<vector<int>> visited(MAX, vector<int>(MAX, 0));
+vector<pair<int, int>> land;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
-int maxVal(){
-    int ret = -1;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            ret = max(ret, visited[i][j]);
-        }
-    }
-    return ret;
+void clearVisited(){
+    for(int i = 0; i < N; i++) fill(visited[i].begin(), visited[i].begin() + M, 0);
 }
 
-void bfs(int y, int x){
+void bfs(pair<int, int> pos){
     queue<pair<int, int>> q;
-    q.push(make_pair(y, x));
+    q.push(pos);
+    visited[pos.first][pos.second] = 1;
     while(!q.empty()){
-        y = q.front().first;
-        x = q.front().second;
+        int x = q.front().second;
+        int y = q.front().first;
         q.pop();
         for(int i = 0; i < 4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
-            if(MAP[ny][nx] == 'L' && visited[ny][nx] == 0){
-                visited[ny][nx] = visited[y][x] + 1;
-                q.push(make_pair(ny, nx));
-            }
+            if(nx < 0 || ny < 0 || nx >= M || ny >= N || visited[ny][nx] || MAP[ny][nx] == 'W') continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            ans = max(ans, visited[ny][nx]);
+            q.push(make_pair(ny, nx));
         }
     }
-}
 
-int solve(int y, int x){
-    bfs(y, x);
-    return maxVal();
 }
-
-void clearVisited(){
-    for(int i = 0; i < N; i++){
-        fill(visited[i].begin(), visited[i].begin() + M, 0);
-    }
-}
-
 int main(){
     cin >> N >> M;
     for(int i = 0; i < N; i++){
         cin >> input;
         for(int j = 0; j < M; j++){
             MAP[i][j] = input[j];
+            if(MAP[i][j] == 'L') land.push_back(make_pair(i, j));
         }
     }
 
-    int res = 0;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            if(MAP[i][j] == 'L'){
-                clearVisited();
-                visited[i][j] = 1;  // 이걸 지우고 res를 출력하면 오답ㅠㅠㅠ
-                res = max(res, solve(i, j));
-            }
-        }
+    for(int i = 0; i < land.size(); i++){
+        bfs(land[i]);
+        clearVisited();
     }
-    cout << res - 1 << "\n";
+    cout << ans - 1 << "\n";
 }
