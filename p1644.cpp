73bb2d3bbc2e0a6272b6e99011593563d8803855
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-07-28 13:20:11 +0900

    p1644

--- p1644.cpp
+++ p1644.cpp
@@ -1,30 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N, ans, sum;
-vector<int> che;
-vector<int> go(int max){
-    vector<int> v;
-    vector<bool> c(max, false);
-    for(int i = 2; i <= max; i++){
-        if(c[i]) continue;
-        for(int j = 2 * i; j <= max; j += i){
-            c[j] = true;
-        }
-    }
-    for(int i = 2; i <= max; i++) if(!c[i]) v.push_back(i);
-    return v;
-}
+#define MAX 4000004
+int N, ans, sum, p;
+vector<int> v;
+vector<bool> che(MAX, false);
 int main(){
     cin >> N;
-    che = go(N);
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for(int i = 0; i < che.size(); i++){
-        sum += che[i];
-        pq.push(che[i]);
-        while(sum > N){
-            sum -= pq.top();
-            pq.pop();
-        }
+    for(int i = 2; i <= N; i++){
+        if(che[i]) continue;
+        for(int j = i * 2; j <= N; j += i) che[j] = true;
+    }
+    for(int i = 2; i <= N; i++) if(che[i] == false) v.push_back(i);
+    for(int i = 0; i < v.size(); i++){
+        sum += v[i];
+        while(sum > N) sum -= v[p++];
         if(sum == N) ans++;
     }
     cout << ans << "\n";
