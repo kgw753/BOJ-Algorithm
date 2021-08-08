d7461896fdb8d571354ba8f4fb74ef6864eb9031
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-08-08 11:57:18 +0900

    p1700

--- p1700.cpp
+++ p1700.cpp
@@ -1,48 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 104
-int N, K, ans;
-vector<int> v(MAX, 0);
-vector<queue<int>> check(MAX, queue<int>());
-queue<int> q;
-int go(vector<int> soc){
-    int idx = 0, m = -1;
-    for(int i = 1; i <= K; i++){
-        if(check[i].empty()){
-            idx = i;
-            break;
-        }
-        else if(check[i].front() > m){
-            idx = i;
-            m = check[i].front();
-        }
-    }
-    return idx;
-}
-bool notHave(vector<int> soc, int item){
-    for(int i = 0; i < soc.size(); i++){
-        if(soc[i] == item) return false;
-    }
-    return true;
-}
+int N, K, cnt;
+vector<int> v(MAX, 0), visited(MAX, 0), sock;
 int main(){
     cin >> N >> K;
     for(int i = 0; i < K; i++){
         cin >> v[i];
-        check[v[i]].push(i);
     }
-    vector<int> soc(N, 0);
     for(int i = 0; i < K; i++){
-        int idx = i;
-        if(soc.size() == N && notHave(soc, v[i])){
-            idx = go(soc);
-            check[idx].pop();
-            ans++;
+        if(visited[v[i]]) continue;
+        if(sock.size() == N){
+            int item = sock[0], idx = 0;
+            for(int s : sock) {
+                if(find(v.begin() + i + 1, v.end(), s) == v.end()){
+                    item = s;
+                    continue;
+                }
+                for(int j = i + 1; j < K; j++){
+                    if(s == v[j] && idx < j){
+                        idx = j;
+                        item = s;
+                    }
+                }
+            }
+            sock.erase(find(sock.begin(), sock.end(), item));
+            visited[item] = 0;
+            cnt++;
+            cout << i << " : " << item << "\n";
         }
-        else{
-            check[v[idx]].pop();
-        }
-        soc.push_back(v[i]);
+        sock.push_back(v[i]);
+        visited[v[i]] = 1;
     }
-    cout << ans << "\n";
+    cout << cnt << "\n";
 }
