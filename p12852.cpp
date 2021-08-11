3caf532d6f2e66b3732e35d9589cb0c4c13804b3
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-08-11 17:02:21 +0900

    p12852

--- p12852.cpp
+++ p12852.cpp
@@ -1,31 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 1000004
-int N, prev, cnt;
-int dp[MAX], prevIdx[MAX]; // prevIdx ¿˙¿Â
-vector<int> v;
+int N;
+vector<int> dp(MAX, 1e9);
 void print(int idx){
-    if(idx == 0) return;
-    cnt++;
-    v.push_back(idx);
-    print(prevIdx[idx]);
+    cout << idx << " ";
+    if(idx == 1) return;
+    if(idx % 3 == 0 && dp[idx] == dp[idx / 3] + 1) print(idx / 3);
+    else if(idx % 2 == 0 && dp[idx] == dp[idx / 2] + 1) print(idx / 2);
+    else if(dp[idx] == dp[idx - 1] + 1) print(idx - 1);
 }
 int main(){
     cin >> N;
-    memset(prevIdx, 0, sizeof(prevIdx));
+    dp[1] = 0;
     for(int i = 2; i <= N; i++){
-        int cmp, d;
-        if(i % 3 == 0 && i % 2 == 0) cmp = min(dp[i / 2], dp[i / 3]), d = (dp[i / 2] < dp[i / 3]) ? 2 : 3;
-        else if(i % 3 == 0) cmp = dp[i / 3], d = 3;
-        else if(i % 2 == 0) cmp = dp[i / 2], d = 2;
-        else cmp = 1e9, d = 0;
-        if(d == 0) prevIdx[i] = i - 1;
-        else prevIdx[i] = (dp[i / d] > dp[i - 1]) ? i - 1 : i / d;
-        dp[i] = min(cmp, dp[i - 1]) + 1;
-        
+        if(i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+        if(i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        dp[i] = min(dp[i], dp[i - 1] + 1);
     }
-    print(N);
     cout << dp[N] << "\n";
-    for(int i : v) cout << i << " ";
+    print(N);
     cout << "\n";
 }
