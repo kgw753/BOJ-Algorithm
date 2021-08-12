7a7c9986540938a9b355da9d2747fc049b156849
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-08-12 12:38:01 +0900

    p1003

--- p1003.cpp
+++ p1003.cpp
@@ -1,27 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define MAX 44
+int T, N;
+vector<pair<int, int>> dp(MAX);
 int main(){
-    int T;
-    int max = 41;
-    vector<vector<int>> dp(max, vector<int>(2, 0));
-    vector<int> numbers;
     cin >> T;
-
-    dp[0][0] = 1;
-    dp[1][1] = 1;
-
-    for(int i = 2; i < max; i++){
-        dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
-        dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+    dp[0] = {1, 0};
+    dp[1] = {0, 1};
+    for(int i = 2; i <= MAX; i++){
+        dp[i].first = dp[i - 1].first + dp[i - 2].first;
+        dp[i].second = dp[i - 1].second + dp[i - 2].second;
     }
-    
-    for(int i = 0; i < T; i++){
-        int num;
-        cin >> num;
-        numbers.push_back(num);
-    }
-
-    for(int i = 0; i < numbers.size(); i++){
-        cout << dp[numbers[i]][0] << " " << dp[numbers[i]][1] << "\n";
+    while(T--){
+        cin >> N;
+        cout << dp[N].first << " " << dp[N].second << "\n";
     }
 }
