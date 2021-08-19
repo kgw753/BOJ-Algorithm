91878cf08b6eb90337f4a9c3683b3498a890764e
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-08-19 20:30:40 +0900

    p14867

--- p14867.cpp
+++ p14867.cpp
@@ -1,40 +1,51 @@
-#include <iostream>
-#include <map>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
-
-int a, b, c, d;
-map< pair<int, int>,int > m;
+#define MAX 1004
+int A, B, C, D;
+map<pair<int, int>, int> visited;
 queue<pair<int, int>> q;
-
-void enqueue(int x,int y,int d) { 
-    if (m[{x, y}]) return; 
-    m[{x, y}] = d+1;
-    q.push({ x,y });
-}
-
-int bfs(int x, int y) {
-    m[{x, y}] = 1;
-    q.push({ x,y });
-    while (!q.empty()) {
-        x = q.front().first;
-        y = q.front().second;
+int bfs(int x, int y){
+    q.push({x, y});
+    visited[{x, y}] = 1;
+    while(q.size()){
+        int a = q.front().first;
+        int b = q.front().second;
         q.pop();
-
-        enqueue(a, y, m[{x, y}]);
-        enqueue(x, b, m[{x, y}]);
-        enqueue(0, y, m[{x, y}]);
-        enqueue(x, 0, m[{x, y}]);
-		enqueue(min(x + y, a), max(0, x + y - a), m[{x, y}]);
-		enqueue(max(0, x + y - b), min(x + y, b), m[{x, y}]); 
+        if(!visited[{A, b}]){
+            q.push({A, b});
+            visited[{A, b}] = visited[{a, b}] + 1;
+            if(A == C && b == D) break;
+        } 
+        if(!visited[{a, B}]){
+            q.push({a, B});
+            visited[{a, B}] = visited[{a, b}] + 1;
+            if(a == C && B == D) break;
+        }
+        if(visited[{0, b}] == 0){
+            q.push({0, b});
+            visited[{0, b}] = visited[{a, b}] + 1; 
+            if(0 == C && b == D) break;
+        }
+        if(visited[{a, 0}] == 0){
+            q.push({a, 0});
+            visited[{a, 0}] = visited[{a, b}] + 1;
+            if(a == C && 0 == D) break;
+        }
+        if(visited[{min(a + b, A), max(0, b - (A - a))}] == 0){
+            q.push({min(a + b, A), max(0, b - (A - a))});
+            visited[{min(a + b, A), max(0, b - (A - a))}] = visited[{a, b}] + 1;
+            if(min(a + b, A) == C && max(0, b - (A - a)) == D) break;
+        }
+        if(visited[{max(0, a - (B - b)), min(a + b, B)}] == 0){
+            q.push({max(0, a - (B - b)), min(a + b, B)});
+            visited[{max(0, a - (B - b)), min(a + b, B)}] = visited[{a, b}] + 1;
+            if(max(0, a - (B - b)) == C && min(a + b, B) == D) break;
+        }
     }
-    if (m[{c, d}]) return m[{c, d}]-1;
+    if(visited[{C, D}]) return visited[{C, D}] - 1;
     else return -1;
 }
-
-int main()
-{
-    cin >> a >> b >> c >> d;
-    
-    cout<<bfs(0, 0);
+int main(){
+    cin >> A >> B >> C >> D;
+    cout << bfs(0, 0) << "\n";
 }
