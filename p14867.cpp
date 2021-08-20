3a6a2604b249ce7fe487154b4e7e2cb2767c5a45
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-08-20 15:43:16 +0900

    p14867

--- p14867.cpp
+++ p14867.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1004
 int A, B, C, D;
 map<pair<int, int>, int> visited;
 queue<pair<int, int>> q;
+void enqueue(int a, int b, int prev){
+    if(visited[{a, b}]) return;
+    q.push({a, b});
+    visited[{a, b}] = prev + 1;
+}
 int bfs(int x, int y){
     q.push({x, y});
     visited[{x, y}] = 1;
@@ -11,36 +15,15 @@ int bfs(int x, int y){
         int a = q.front().first;
         int b = q.front().second;
         q.pop();
-        if(!visited[{A, b}]){
-            q.push({A, b});
-            visited[{A, b}] = visited[{a, b}] + 1;
-            if(A == C && b == D) break;
-        } 
-        if(!visited[{a, B}]){
-            q.push({a, B});
-            visited[{a, B}] = visited[{a, b}] + 1;
-            if(a == C && B == D) break;
-        }
-        if(visited[{0, b}] == 0){
-            q.push({0, b});
-            visited[{0, b}] = visited[{a, b}] + 1; 
-            if(0 == C && b == D) break;
-        }
-        if(visited[{a, 0}] == 0){
-            q.push({a, 0});
-            visited[{a, 0}] = visited[{a, b}] + 1;
-            if(a == C && 0 == D) break;
-        }
-        if(visited[{min(a + b, A), max(0, b - (A - a))}] == 0){
-            q.push({min(a + b, A), max(0, b - (A - a))});
-            visited[{min(a + b, A), max(0, b - (A - a))}] = visited[{a, b}] + 1;
-            if(min(a + b, A) == C && max(0, b - (A - a)) == D) break;
-        }
-        if(visited[{max(0, a - (B - b)), min(a + b, B)}] == 0){
-            q.push({max(0, a - (B - b)), min(a + b, B)});
-            visited[{max(0, a - (B - b)), min(a + b, B)}] = visited[{a, b}] + 1;
-            if(max(0, a - (B - b)) == C && min(a + b, B) == D) break;
-        }
+        // Fill
+        enqueue(0, b, visited[{a, b}]);
+        enqueue(a, 0, visited[{a, b}]);
+        // Empty
+        enqueue(A, b, visited[{a, b}]);
+        enqueue(a, B, visited[{a, b}]);
+        // Move
+        enqueue(max(0, a - (B - b)), min(B, b + a), visited[{a, b}]);
+        enqueue(min(A, a + b), max(0, b - (A - a)), visited[{a, b}]);
     }
     if(visited[{C, D}]) return visited[{C, D}] - 1;
     else return -1;
