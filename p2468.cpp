9f7fff605d5f100b22213f576f4a00c02f6be8cf
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-01 18:02:52 +0900

    p2468

--- p2468.cpp
+++ p2468.cpp
@@ -1,97 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 101
-int N, m, M;
-int area;
+#define MAX 104
+int N, mn = 1e9, mx = -1, MAP[MAX][MAX], visited[MAX][MAX], res = 1;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
-
-vector<vector<int>> MAP(MAX, vector<int>(MAX, 0));
-vector<vector<bool>> visited(MAX, vector<bool>(MAX, false));
-vector<vector<bool>> counter(MAX, vector<bool>(MAX, false));
-
-void print(vector<vector<bool>> v){
-    cout << "\n";
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << v[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    cout << "\n";
-}
-
-void bfs(int h, int y, int x){
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
-    visited[y][x] = true;
-
-    while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
-
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if(MAP[ny][nx] == h && visited[ny][nx] == false){
-                q.push(make_pair(nx, ny));
-                visited[ny][nx] = true;
-            }
-        }
-    }
-}
-
-void areaCnt(int x, int y){
-    counter[y][x] = true;
-
+void dfs(int y, int x, int h){
     for(int i = 0; i < 4; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
-
-        if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-        if(counter[ny][nx] == false){
-            areaCnt(nx, ny);
-        }
-        
+        if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+        if(MAP[ny][nx] <= h || visited[ny][nx]) continue;
+        visited[ny][nx] = 1;
+        dfs(ny, nx, h);
     }
 }
-
+int go(int h){
+    int ret = 0;
+    memset(visited, 0, sizeof(visited));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(MAP[i][j] > h && visited[i][j] == 0){
+                visited[i][j] = 1;
+                dfs(i, j, h);
+                ret++;
+            }
+        }
+    }
+    return ret;
+}
 int main(){
     cin >> N;
-    m = MAX;
-    M = 0;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cin >> MAP[i][j];
-            m = min(m, MAP[i][j]);
-            M = max(M, MAP[i][j]);
+            mn = min(mn, MAP[i][j]);
+            mx = max(mx, MAP[i][j]);
         }
     }
-
-    area = 1;
-    for(int i = m; i <= M; i++){
-        for(int j = 0; j < N; j++){
-            for(int k = 0; k < N; k++){
-                if(MAP[j][k] == i && visited[j][k] == false){
-                    bfs(i, j, k);
-                }
-            }
-        }
-
-        int cnt = 0;
-        counter = visited;
-        for(int j = 0; j < N; j++){
-            for(int k = 0; k < N; k++){
-                if(counter[k][j] == false){
-                    areaCnt(j, k);
-                    cnt++;
-                }
-            }
-        }
-
-        area = max(area, cnt);
+    for(int i = mn; i < mx; i++){
+        res = max(res, go(i));
     }
-    cout << area << "\n";
+    cout << res << "\n";
 }
