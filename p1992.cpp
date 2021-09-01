912d9a865b0a9a02ee534aa59a17d98d956b4d21
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-01 18:20:06 +0900

    p1992

--- p1992.cpp
+++ p1992.cpp
@@ -1,41 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 65
-int N;
-vector<vector<int>> MAP(MAX, vector<int>(MAX, 0));
-string input, result;
-
-void search(int r, int c, int size){
-    if(size == 1){
-        result += (char)(MAP[r][c] + '0');
-        return;
-    }
-    
-    bool same = true;
-    int tmp = MAP[r][c];
-    for(int i = r; i < size + r; i++){
-        for(int j = c; j < size + c; j++){
-            if(tmp != MAP[i][j]){
-                same = false;
-                break;
-            }
+#define MAX 68
+int N, MAP[MAX][MAX];
+string input;
+string go(int y, int x, int size){
+    int val = MAP[y][x];
+    for(int i = y; i < y + size; i++){
+        for(int j = x; j < x + size; j++){
+            if(val != MAP[i][j]) val = -1;
         }
     }
-
-    if(same){
-        result += (char)(tmp + '0');
-    }
-    else{
-        size /= 2;
-        result += '(';
-        search(r, c, size);
-        search(r, c + size, size);
-        search(r + size, c, size);
-        search(r + size, c + size, size);
-        result += ')';
-    }
+    if(val != -1) return to_string(val);
+    size /= 2;
+    string ret = "";
+    ret += "(" + go(y, x, size);
+    ret += go(y, x + size, size);
+    ret += go(y + size, x, size);
+    ret += go(y + size, x + size, size) + ")";
+    return ret;
 }
-
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
@@ -44,7 +27,5 @@ int main(){
             MAP[i][j] = input[j] - '0';
         }
     }
-
-    search(0, 0, N);
-    cout << result << "\n";
+    cout << go(0, 0, N) << "\n";
 }
