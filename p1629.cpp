ca1a0321b019b68a6c2c9e1a3663bedbbacad5bf
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-01 11:48:50 +0900

    p1629

--- p1629.cpp
+++ p1629.cpp
@@ -4,10 +4,10 @@ typedef long long ll;
 ll a, b, c;
 ll go(ll a, ll b){
     if(b == 1) return a % c;
-    ll tmp = go(a, b / 2) % c;
-    tmp = tmp * tmp % c;
-    if(b % 2) tmp = tmp * a % c;
-    return tmp;
+    ll ret = go(a, b / 2);
+    ret = ret * ret % c;
+    if(b % 2) ret = ret * a % c;
+    return ret;
 }
 int main(){
     cin >> a >> b >> c;
