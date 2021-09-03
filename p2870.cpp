fc4e37da75c3520d32835ba932aad33d705223a7
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-03 12:52:33 +0900

    p2870

--- p2870.cpp
+++ p2870.cpp
@@ -1,32 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N;
-string input, res;
-vector<string> answer;
-
+int N, flag;
+string input, num;
+vector<string> v;
 bool cmp(string s1, string s2){
     if(s1.length() == s2.length()) return s1 < s2;
     return s1.length() < s2.length();
 }
-
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
         cin >> input;
-        int j = 0;
-        while(j < input.length()){
-            if(isdigit(input[j])){
-                while(j < input.length() && isdigit(input[j])){
-                    res += input[j++];
-                }
-                while(res.length() && res.front() == '0') res.erase(res.begin());
-                if(res.length() == 0) res = "0";
-                answer.push_back(res);
-                res = "";
+        flag = 0;
+        num = "";
+        for(char c : input){
+            if(c <= '9' && c >= '0'){
+                flag = 1;
+                if(c == '0' && num.empty()) continue;
+                else num += c;
             }
-            j++;
+            else if(flag){
+                if(num.empty()) v.push_back("0");
+                else v.push_back(num);
+                num = "";
+                flag = 0;
+            }
+        }
+        if(flag) {
+            if(num.empty()) v.push_back("0");
+            else v.push_back(num);
         }
     }
-    sort(answer.begin(), answer.end(), cmp);
-    for(string ans: answer) cout << ans << "\n";
+    sort(v.begin(), v.end(), cmp);
+    for(string str : v) cout << str << "\n";
 }
