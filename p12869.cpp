2c0de96786b3d7fafb19855a17021c7951d4a8c1
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-03 15:20:26 +0900

    p12869

--- p12869.cpp
+++ p12869.cpp
@@ -1,33 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 3
-int N, ans = 1e9;
-vector<int> scv(MAX, 0);
-
+int N, scv[3], cnt = 1e9;
 bool allDie(){
-    for(int i = 0; i < scv.size(); i++) 
+    for(int i = 0; i < N; i++){
         if(scv[i] > 0) return false;
+    }
     return true;
 }
-
-void dfs(int depth, vector<int> dmg){
+void attack(int depth, vector<int> dmg){
     if(allDie()){
-        ans = min(ans, depth);
+        cnt = min(cnt, depth);
         return;
     }
     do{
         for(int i = 0; i < 3; i++) scv[i] -= dmg[i];
-        dfs(depth + 1, dmg);
+        attack(depth + 1, dmg);
         for(int i = 0; i < 3; i++) scv[i] += dmg[i];
     }while(next_permutation(dmg.begin(), dmg.end()));
 }
 int main(){
     cin >> N;
-    for(int i = 0; i < N; i++){
-        cin >> scv[i];
-    }
-    vector<int> dmg;
-    for(int i = 1; i <= 9; i *= 3) dmg.push_back(i);
-    dfs(0, dmg);
-    cout << ans << "\n";
+    for(int i = 0; i < N; i++) cin >> scv[i];
+    attack(0, {1, 3, 9});
+    cout << cnt << "\n";
 }
