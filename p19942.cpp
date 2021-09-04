bf8389a57e577233679d2c8b18409eee2ac39894
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-04 18:02:13 +0900

    p19942

--- p19942.cpp
+++ p19942.cpp
@@ -1,50 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 16
-int N, ans = 1e9;
-int mp, mf, ms, mv;
-struct A{
-    int mp, mf, ms, mv, cost;
-}arr[MAX];
-
-map<int, vector<vector<int>>> res;
-
+int N, mp, mf, ms, mv, ans = 1e9;
+int p, f, s, v, c;
+struct Food{
+    int p, f, s, v, c;
+};
+vector<Food> fd(19);
+vector<int> idxs;
+bool check(string tmp){
+    if(idxs.empty()) return true;
+    return string(idxs.begin(), idxs.end()) > tmp;
+}
 int main(){
-    cin >> N;
-    cin >> mp >> mf >> ms >> mv;
-    for(int i = 0 ; i < N; i++){
-        cin >> arr[i].mp >> arr[i].mf >> arr[i].ms >> arr[i].mv >> arr[i].cost;
+    cin >> N >> mp >> mf >> ms >> mv;
+    for(int i = 0; i < N; i++){
+        cin >> fd[i].p >> fd[i].f >> fd[i].s >> fd[i].v >> fd[i].c;
     }
-
     for(int i = 1; i < (1 << N); i++){
-        vector<int> idx;
-        int p, f, s, v, c;
+        vector<int> tmp;
         p = f = s = v = c = 0;
         for(int j = 0; j < N; j++){
             if(i & (1 << j)){
-                idx.push_back(j + 1);
-                p += arr[j].mp;
-                f += arr[j].mf;
-                s += arr[j].ms;
-                v += arr[j].mv;
-                c += arr[j].cost;
+                tmp.push_back(j + 1);
+                p += fd[j].p;
+                f += fd[j].f;
+                s += fd[j].s;
+                v += fd[j].v;
+                c += fd[j].c;
             }
         }
-
-        if(p >= mp && f >= mf && s >= ms && v >= mv){
-            if(ans >= c){
-                ans = c;
-                res[c].push_back(idx);
-            }
+        if(p < mp || f < mf || s < ms || v < mv) continue;
+        if(ans > c || (ans == c && check(string(tmp.begin(), tmp.end())))){
+            idxs = tmp;
+            ans = c;
         }
     }
     if(ans == 1e9) cout << -1 << "\n";
     else{
         cout << ans << "\n";
-        sort(res[ans].begin(), res[ans].end());
-        for(int i = 0; i < res[ans][0].size(); i++){
-            cout << res[ans][0][i] << " ";
-        }
+        for(int i : idxs) cout << i << " ";
         cout << "\n";
     }
 }
