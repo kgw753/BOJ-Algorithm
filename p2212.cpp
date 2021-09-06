91000468c23ad10cd2bc98a26db256b738e149a6
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-06 14:04:54 +0900

    p2212

--- p2212.cpp
+++ p2212.cpp
@@ -1,32 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+#define MAX 10004
+int N, K, ans;
+vector<int> v(MAX, 0), dist(MAX, 0);
 int main(){
-    int N, K;
     cin >> N >> K;
-
-    if(K >= N){
-      cout << 0 << "\n";
-      return 0;  
-    } 
-    vector<int> sensors(N, 0);
-    vector<int> gap(N - 1, 0);
-
-    for(int i = 0; i < N; i++){
-        cin >> sensors[i];
-    }
-
-    sort(sensors.begin(), sensors.end());
-
-    for(int i = 0; i < N - 1; i++){
-        gap[i] = sensors[i + 1] - sensors[i];
-    }
-    
-    sort(gap.begin(), gap.end());
-    int answer = 0;
-    for(int i = 0; i < gap.size() - (K - 1); i++){
-        answer += gap[i];
-    }
-
-    cout << answer << "\n";
+    for(int i = 0; i < N; i++) cin >> v[i];
+    sort(v.begin(), v.begin() + N);
+    for(int i = 0; i < N - 1; i++) dist[i] = v[i + 1] - v[i];
+    sort(dist.begin(), dist.begin() + (N - 1));
+    for(int i = 0; i < N - 1 - (K - 1); i++) ans += dist[i];
+    cout << ans << "\n";
 }
