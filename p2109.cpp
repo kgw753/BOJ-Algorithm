cf2fd05bbef3a7c5a7a3a43a9885e791eb97016b
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-14 17:33:59 +0900

    p2109

--- p2109.cpp
+++ p2109.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N, p, d, ans;
+int N, p, d;
 vector<pair<int, int>> v;
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
         cin >> p >> d;
-        v.push_back(make_pair(d, p));
+        v.push_back({d, p});
     }
     sort(v.begin(), v.end());
     priority_queue<int, vector<int>, greater<int>> pq;
@@ -14,9 +14,10 @@ int main(){
         pq.push(v[i].second);
         if(pq.size() > v[i].first) pq.pop();
     }
+    int pay = 0;
     while(pq.size()){
-        ans += pq.top();
+        pay += pq.top();
         pq.pop();
-    } 
-    cout << ans << "\n";
+    }
+    cout << pay << "\n";
 }
