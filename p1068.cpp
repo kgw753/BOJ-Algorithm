6374bac62f76bd06b20b1145b02b5443962af362
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-15 14:49:19 +0900

    p1068

--- p1068.cpp
+++ p1068.cpp
@@ -1,47 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 51
-int N, input, cnt;
-vector<vector<int>> v(MAX, vector<int>());
-vector<bool> visited(MAX, false);
-
-void dfs(int idx){
-    if(visited[idx]) return;
-    visited[idx] = true;
-    if(v[idx].size() == 0) cnt++;
-    for(int i = 0; i < v[idx].size(); i++){
-        dfs(v[idx][i]);
+#define MAX 54
+int N, root, node, del, cnt;
+vector<vector<int>> MAP(MAX, vector<int>());
+void go(int node){
+    int child = 0;
+    for(int i : MAP[node]){
+        if(i == del) continue;
+        go(i);
+        child++;
     }
+    if(child == 0) cnt++;
 }
-
-void clearing(int idx){
-    for(int i = 0; i < v[idx].size(); i++){
-        clearing(v[idx][i]);
-    }
-    v[idx].clear();
-}
-
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
-        cin >> input;
-        if(input == -1) continue;
-        v[input].push_back(i);
+        cin >> node;
+        if(node == -1) root = i;
+        else MAP[node].push_back(i);
     }
-
-    cin >> input;
-    clearing(input);
-    for(int i = 0; i < N; i++){
-        auto del = find(v[i].begin(), v[i].end(), input);
-        if(del != v[i].end()) v[i].erase(del);
+    cin >> del;
+    if(del == root) cout << 0 << "\n";
+    else{
+        go(root);
+        cout << cnt << "\n";
     }
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < v[i].size(); j++){
-            dfs(v[i][j]);
-        }
-    }
-
-    if(N == 2) cnt = 1;
-    cout << cnt << "\n";
 }
