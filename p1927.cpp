fd952bd5c39440206e597081b1d3146135b8e90e
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-21 17:54:15 +0900

    p1927

--- p1927.cpp
+++ p1927.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int N, input;
 priority_queue<int, vector<int>, greater<int>> pq;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> N;
     for(int i = 0; i < N; i++){
         cin >> input;
