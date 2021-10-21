77d25e2777d558af7ea18db63e4d0cc1aa1c7830
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-21 18:40:49 +0900

    p15686

--- p15686.cpp
+++ p15686.cpp
@@ -1,51 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 50
-int N, M;
-vector<vector<int>> MAP(MAX, vector<int>(MAX, 0));
-vector<vector<int>> chickenIDX;
-vector<pair<int, int>> chicken, house;
-vector<int> hdist(MAX * 2, 1e9);
-vector<int> dist(MAX * MAX, 0);
-
-void combi(int k, vector<int> v){
-    if(v.size() == M) {
-        chickenIDX.push_back(v);
-        return;
+#define MAX 54
+int N, M, input, ans = 1e9;
+int MAP[MAX][MAX];
+vector<pair<int, int>> chick, hom;
+int getChickDist(int idx, int bm){
+    int ret = 1e9;
+    for(int i = 0; i < chick.size(); i++){
+        if((1 << i) & bm){
+            int tmp = abs(hom[idx].first - chick[i].first) + abs(hom[idx].second - chick[i].second);
+            ret = min(ret, tmp);
+        }
     }
-    for(int i = k; i < chicken.size(); i++){
-        v.push_back(i);
-        combi(i + 1, v);
-        v.pop_back();
-    }
+    return ret;
 }
-
+int getDist(int bm){
+    int dist = 0;
+    for(int i = 0; i < hom.size(); i++){
+        dist += getChickDist(i, bm);
+    }
+    return dist;
+}
+bool chickCnt(int bm){
+    int cnt = 0;
+    while(bm > 0){
+        if(bm % 2) cnt++;
+        bm /= 2;
+    }
+    return cnt > M;
+}
 int main(){
     cin >> N >> M;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cin >> MAP[i][j];
-            if(MAP[i][j] == 1) house.push_back(make_pair(i, j));
-            else if(MAP[i][j] == 2) chicken.push_back(make_pair(i, j));
-        }
-    }
-
-    combi(0, vector<int>());
-
-    for(int i = 0; i < chickenIDX.size(); i++){
-        fill(hdist.begin(), hdist.begin() + house.size(), 1e9); 
-        for(int j = 0; j < chickenIDX[i].size(); j++){
-            int y = chicken[chickenIDX[i][j]].first;
-            int x = chicken[chickenIDX[i][j]].second;
-            for(int k = 0; k < house.size(); k++){
-                int dy = house[k].first;
-                int dx = house[k].second;
-                hdist[k] = min(hdist[k], abs(x - dx) + abs(y - dy));
+            if(MAP[i][j]){
+                if(MAP[i][j] == 1) hom.push_back({i, j});
+                else if(MAP[i][j] == 2) chick.push_back({i, j});
             }
         }
-        for(int j = 0; j < house.size(); j++) dist[i] += hdist[j];
     }
-
-    sort(dist.begin(), dist.begin() + chickenIDX.size());
-    cout << dist[0] << "\n";
+    for(int i = 1; i < (1 << chick.size()); i++){
+        int tmp = 0;
+        if(chickCnt(i)) continue;
+        ans = min(ans, getDist(i));
+    }
+    cout << ans << "\n";
 }
