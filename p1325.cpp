1d2a54cd4a3065a591a8afe0acbd7985ec12b85d
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-22 16:01:03 +0900

    p1325

--- p1325.cpp
+++ p1325.cpp
@@ -1,30 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 10004
-int N, M, a, b, visited[MAX], answer[MAX];
-vector<vector<int>> MAP(MAX, vector<int>());
-int dfs(int idx){
-    visited[idx] = 1;
+int N, M, a, b, mx = -1;
+int cnt[MAX], visited[MAX];
+vector<int> MAP[MAX];
+int dfs(int node){
+    visited[node] = 1;
     int ret = 1;
-    for(int nxt : MAP[idx]){
-        if(visited[nxt]) continue;
-        ret += dfs(nxt);
+    for(int i = 0; i < MAP[node].size(); i++){
+        if(visited[MAP[node][i]]) continue;
+        ret += dfs(MAP[node][i]);
     }
     return ret;
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> N >> M;
     for(int i = 0; i < M; i++){
         cin >> a >> b;
         a--, b--;
         MAP[b].push_back(a);
     }
-    int mx = -1;
     for(int i = 0; i < N; i++){
         memset(visited, 0, sizeof(visited));
-        answer[i] = dfs(i);
-        mx = max(mx, answer[i]);
+        cnt[i] = dfs(i);
+        mx = max(mx, cnt[i]);
     }
-    for(int i = 0; i < N; i++) if(mx == answer[i]) cout << i + 1 << " ";
+    for(int i = 0; i < N; i++){
+        if(cnt[i] == mx) cout << i + 1 << " ";
+        // cout << cnt[i] << "\n";
+    }
     cout << "\n";
 }
