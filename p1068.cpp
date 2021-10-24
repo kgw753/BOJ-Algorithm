171fb18b7c4ebd55ce009cdaaf47f67461fc288b
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-24 13:42:40 +0900

    p1068

--- p1068.cpp
+++ p1068.cpp
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 54
-int N, root, node, del, cnt;
-vector<vector<int>> MAP(MAX, vector<int>());
-void go(int node){
-    int child = 0;
-    for(int i : MAP[node]){
-        if(i == del) continue;
-        go(i);
-        child++;
+int N, parent, root, del;
+vector<int> MAP[MAX];
+int go(int node){
+    int ret = 0;
+    int childCnt = 0;
+    for(int i = 0; i < MAP[node].size(); i++){
+        if(MAP[node][i] == del) continue;
+        ret += go(MAP[node][i]);
+        childCnt++;
     }
-    if(child == 0) cnt++;
+    if(childCnt == 0) return 1;
+    else return ret;
 }
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
-        cin >> node;
-        if(node == -1) root = i;
-        else MAP[node].push_back(i);
+        cin >> parent;
+        if(parent == -1) root = i;
+        else MAP[parent].push_back(i);
     }
     cin >> del;
     if(del == root) cout << 0 << "\n";
-    else{
-        go(root);
-        cout << cnt << "\n";
-    }
+    else cout << go(root) << "\n";
 }
