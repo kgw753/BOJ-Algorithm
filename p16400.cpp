26216da6fd415e460f71ec6fd37d3319815ed4e4
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-10-24 13:28:40 +0900

    p16400

--- p16400.cpp
+++ p16400.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 40001
-int N, dp[MAX];
-bool che[MAX];
-vector<int> prime;
+#define MAX 40004
+const int DIV = 123456789;
+int N, che[MAX];
+vector<int> prime, dp(MAX);
 int main(){
     for(int i = 2; i < MAX; i++){
         if(che[i]) continue;
@@ -12,13 +12,12 @@ int main(){
             che[j] = true;
         }
     }
+    cin >> N;
     dp[0] = 1;
     for(int p : prime){
         for(int i = p; i < MAX; i++){
-            dp[i] += dp[i - p];
-            dp[i] %= 123456789;
+            dp[i] = (dp[i] + dp[i - p]) % DIV;
         }
     }
-    cin >> N;
     cout << dp[N] << "\n";
 }
