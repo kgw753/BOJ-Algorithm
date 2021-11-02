7a2bf31fcf1d4887a428a6eaba04fa4eed6ed930
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-02 16:12:30 +0900

    p2133

--- p2133.cpp
+++ p2133.cpp
@@ -1,15 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 32
-int N, dp[MAX];
+#define MAX 34
+int N;
+int dp[MAX];
 int main(){
-    cin >> N;
-    dp[2] = 3;
-    for(int i = 4; i < MAX; i += 2){
-        dp[i] = dp[i - 2] + 2;
-        for(int j = i - 2; j > 0; j -= 2){
-            dp[i] += dp[j] * 2;
-        }
-    }
-    cout << dp[N] << "\n";
+	cin >> N;
+	dp[2] = 3;
+	for(int i = 4; i <= N; i += 2){
+		dp[i] = dp[i - 2] + 2;
+		for(int j = 2; j <= i - 2; j += 2){
+			dp[i] += dp[j] * 2;
+		}
+	}
+	cout << dp[N] << "\n";
 }
