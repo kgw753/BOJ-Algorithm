5a5b689eb635aebcbf1eef351d9c081e2357b554
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-04 14:48:02 +0900

    p9656

--- p9656.cpp
+++ p9656.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 1004
-int N, dp[MAX];
+int N;
+int dp[MAX];
 int main(){
-    cin >> N;
-    dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 0;
-    for(int i = 4; i <= N; i++){
-        if(dp[i - 1] || dp[i - 3]) dp[i] = 0;
-        else dp[i] = 1;
-    }
-    if(dp[N]) cout << "SK\n";
-    else cout << "CY\n";
+	cin >> N;
+	dp[1] = dp[3] = 0;
+	dp[2] = 1;
+	for(int i = 4; i <= N; i++){
+		if(dp[i - 1] == dp[i - 3]) dp[i] = !dp[i - 1];
+        else dp[i] = i % 2;
+	}
+	if(dp[N]) cout << "SK\n";
+	else cout << "CY\n";
 }
