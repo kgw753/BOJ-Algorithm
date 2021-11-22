5b8b6523a1ddd0e358ed4960060e8816bd2733c3
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-22 17:53:56 +0900

    p2240

--- p2240.cpp
+++ p2240.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1004
+#define MAX_SEC 1004
 #define MAX_MOV 34
 int T, W;
-vector<int> v(MAX);
-int dp[MAX][2][MAX_MOV];
-int go(int sec, int pos, int mov){
-    if(mov > W || sec == T) return 0;
-    int &ret = dp[sec][pos][mov];
-    if(~ret) return ret;
-    return ret = max(go(sec + 1, pos, mov), go(sec + 1, pos ^ 1, mov + 1)) + (v[sec] == pos);
+int dp[MAX_SEC][MAX_MOV][2];
+int v[MAX_SEC];
+int go(int sec, int mov, int pos){
+	if(sec > T || mov < 0) return 0;
+	int &ret = dp[sec][mov][pos];
+	if(~ret) return ret;
+	return ret = max(go(sec + 1, mov, pos), go(sec + 1, mov - 1, !pos)) + (v[sec] == pos);
 }
 int main(){
-    cin >> T >> W;
-    for(int i = 0; i < T; i++){
-        cin >> v[i];
-        v[i]--;
-    }
-    memset(dp, -1, sizeof(dp));
-    cout << max(go(0, 0, 0), go(0, 1, 1)) << "\n";
+	cin >> T >> W;
+	for(int i = 0; i < T; i++){
+		cin >> v[i];
+		v[i]--;
+	}
+	memset(dp, -1, sizeof(dp));
+	cout << max(go(0, W, 0), go(0, W - 1, 1)) << "\n";
 }
