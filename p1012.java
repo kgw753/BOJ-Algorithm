6b0cb72df71cdf7c27055f91304459699d31df62
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-02 17:36:18 +0900

    p1012

--- p1012.java
+++ p1012.java
@@ -1,70 +1,52 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class p1012 {
-
+    public static int tc;
+    public static int N;
+    public static int M;
     public static int count;
-    public static int[] dy = { -1, 0, 1, 0 };
-    public static int[] dx = { 0, 1, 0, -1 };
-    public static int[][] field = new int[51][51];
-    public static boolean[][] visited = new boolean[51][51];
-
-    public static int M, N, K;
-
-    public static void dfs(int y, int x) {
-        visited[y][x] = true;
-        for (int i = 0; i < dy.length; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || ny >= M || nx < 0 || nx >= N)
-                continue;
-            else if (!visited[ny][nx] && field[ny][nx] == 1) {
-                dfs(ny, nx);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static int[][] map;
+    public static int[] dy = {0, 1, 0, -1};
+    public static int[] dx = {1, 0, -1, 0};
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        int[] answer = new int[tc];
-
-        for (int i = 0; i < tc; i++) {
-            count = 0;
-            for (int j = 0; j < field.length; j++) {
-                Arrays.fill(field[j], 0);
-            }
-            for (int j = 0; j < visited.length; j++) {
-                Arrays.fill(visited[j], false);
-            }
-
+        tc = Integer.parseInt(br.readLine());
+        while(tc-- > 0){
+            int res = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
-            for (int j = 0; j < K; j++) {
+            count = Integer.parseInt(st.nextToken());
+            map = new int[N][M];
+            int x, y;
+            for(int i = 0; i < count; i++){
                 st = new StringTokenizer(br.readLine());
-                int m = Integer.parseInt(st.nextToken());
-                int n = Integer.parseInt(st.nextToken());
-                field[m][n] = 1;
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
             }
-
-            for (int p = 0; p < M; p++) {
-                for (int q = 0; q < N; q++) {
-                    if (field[p][q] == 1 && !visited[p][q]) {
-                        dfs(p, q);
-                        count++;
+            
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    if(map[i][j] == 1) {
+                        dfs(i, j);
+                        res++;
                     }
                 }
             }
-
-            answer[i] = count;
+            System.out.println(res);
         }
-
-        for(int ans: answer) System.out.println(ans);
+    }
+    public static void dfs(int y, int x){
+        map[y][x]++;
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= N || nx >= M || map[ny][nx] != 1) continue;
+            dfs(ny, nx);
+        }
     }
 }
