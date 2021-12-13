1739443c4c2c30c1c1d7515561c8aeb9f1dad60b
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-13 18:42:51 +0900

    p2852

--- p2852.java
+++ p2852.java
@@ -1,51 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class p2852 {
     public static int N;
-    public static int scoreA;
-    public static int scoreB;
-    public static int timeA;
-    public static int timeB;
+    public static int score = 0;
+    public static int prev = 0;
+    public static int now = 0;
+    public static int[] time = new int[2];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(sc.nextLine());
+        
+        for(int i = 0; i < N; i++){
+            String[] input = sc.nextLine().split(" ");
+            now = toInt(input[1]);
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int team, time;
-        int prev = 0, now = 0;
-        while(N-- > 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            team = Integer.parseInt(st.nextToken());
-            now = toInt(st.nextToken());
-            if(scoreA > scoreB){
-                timeA += now - prev;
-            }
-            else if(scoreA < scoreB){
-                timeB += now - prev;
-            }
-            if(team == 1) scoreA++;
-            else scoreB++;
+            if(score > 0) time[0] += (now - prev);
+            else if(score < 0) time[1] += (now - prev);
+            
+            if(Integer.parseInt(input[0]) ==  1) score++;
+            else score--;
+            
             prev = now;
         }
-        if(scoreA > scoreB){
-            timeA += (48 * 60) - prev;
-        }
-        else if(scoreA < scoreB){
-            timeB += (48 * 60) - prev;
-        }
-        printTime(timeA);
-        printTime(timeB);
+        
+        prev = now;
+        now = toInt("48:00");
+        
+        if(score > 0) time[0] += (now - prev);
+        else if(score < 0) time[1] += (now - prev);
+        
+        intPrint(time[0]);
+        intPrint(time[1]);
     }
 
-    public static int toInt(String time){
-        int ret = 0;
-        ret += Integer.parseInt(time.split(":")[0]) * 60;
-        ret += Integer.parseInt(time.split(":")[1]);
+    public static int toInt(String scr){
+        String[] time = scr.split(":");
+        int ret = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
         return ret;
     }
-    public static void printTime(int time){
-        System.out.format("%02d:%02d\n", time / 60, time % 60);
+
+    public static void intPrint(int scr){
+        System.out.format("%02d:%02d\n", scr / 60, scr % 60);
     }
 }
