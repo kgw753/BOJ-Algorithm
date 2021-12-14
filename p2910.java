e79efd74368a8d9d547e119375d04fdc189b39d8
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-14 13:59:32 +0900

    p2910

--- p2910.java
+++ p2910.java
@@ -1,42 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.Scanner;
 
 public class p2910 {
     public static int N;
-    public static int C;
-    public static HashMap<Integer, Integer> hm;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine().split(" ")[0]);
-        hm = new LinkedHashMap<>();
+    public static int M;
+    public static Map<Integer, Integer> map;
+    public static Map<Integer, Integer> seq;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        map = new HashMap<>();
+        seq = new HashMap<>();
+        int cnt = 1;
         for(int i = 0; i < N; i++){
-            int num = Integer.parseInt(st.nextToken());
-            if(hm.containsKey(num)){
-                hm.replace(num, hm.get(num) + 1);
+            int input = sc.nextInt();
+            if(map.containsKey(input)) map.put(input, map.get(input) + 1);
+            else {
+                map.put(input, 1);
+                seq.put(input, cnt++);
             }
-            else hm.put(num, 1);
         }
 
-        ArrayList<Integer> arr = new ArrayList<>(hm.keySet());
+        ArrayList<Integer> arr = new ArrayList<>(map.keySet());
+        
         Collections.sort(arr, new Comparator<Integer>(){
             @Override
             public int compare(Integer a, Integer b){
-                return hm.get(b) - hm.get(a);
+                if(map.get(a) == map.get(b)) return seq.get(a) - seq.get(b);
+                else return map.get(b) - map.get(a);
             }
         });
 
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = 0; j < hm.get(arr.get(i)); j++){
-                System.out.print(arr.get(i) + " ");
+        for(Integer num : arr) {
+            for(int i = 0; i < map.get(num); i++){
+                System.out.print(num + " ");
             }
         }
         System.out.println();
