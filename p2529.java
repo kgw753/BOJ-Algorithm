0e5524a3890daf4f0c03dc5202017197e15f2d70
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-24 13:53:58 +0900

    p2529

--- p2529.java
+++ p2529.java
@@ -1,48 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.List;
 
 public class p2529 {
     public static int N;
-    public static char[] op = new char[10];
+    public static List<Character> op = new ArrayList<>();
+    public static List<String> res = new ArrayList<>();
     public static boolean[] visited = new boolean[10];
-    public static ArrayList<String> answer = new ArrayList<>();
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = Integer.parseInt(sc.nextLine());
-        String[] input = sc.nextLine().split(" ");
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
         for(int i = 0; i < N; i++){
-            op[i] = input[i].charAt(0);
+            op.add(input[i].charAt(0));
         }
+        
+        go("", 0);
 
-        dfs("", 0);
+        Collections.sort(res);
 
-        Collections.sort(answer);
-        System.out.println(answer.get(answer.size() - 1));
-        System.out.println(answer.get(0));
+        System.out.println(res.get(res.size() - 1));
+        System.out.println(res.get(0));
     }
 
-    public static void dfs(String res, int depth){
+    public static void go(String nums, int depth){
         if(depth == N + 1){
-            answer.add(res);
+            res.add(nums);
             return;
         }
         
-        for(char c = '0'; c <= '9'; c++){
-            int idx = c - '0';
-
-            if(visited[idx]) continue;
-            if(res.length() == 0 || isValid(op[res.length() - 1], res.charAt(res.length() - 1) - '0', idx)){
-                visited[idx] = true;
-                dfs(res + c, depth + 1);
-                visited[idx] = false;
+        for(int i = 0; i < 10; i++){
+            char now = (char)(i + '0');
+            if(visited[i]) continue;
+            if(nums.length() == 0 || isValid((int)(nums.charAt(depth - 1) - '0'), i, op.get(depth - 1))){
+                visited[i] = true;
+                go(nums + now, depth + 1);
+                visited[i] = false;
             }
         }
     }
 
-    public static boolean isValid(char op, int a, int b){
-        if(op == '<' && a < b) return true;
-        else if(op == '>' && a > b) return true;
-        return false;
+    public static boolean isValid(int a, int b, char o){
+        if(o == '<') return a < b;
+        else return a > b;
     }
+    
 }
