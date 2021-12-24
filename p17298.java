45cfe5f1e97792c60c99f5e4a20856d97f3f4d36
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-24 12:20:28 +0900

    p17298

--- p17298.java
+++ p17298.java
@@ -8,33 +8,31 @@ import java.util.Stack;
 
 public class p17298 {
     public static int N;
+    public static int[] arr;
     public static Stack<int[]> st = new Stack<>();
-    public static int[] ans;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        ans = new int[N];
-        Arrays.fill(ans, -1);
+
+        arr = new int[N];
+        Arrays.fill(arr, -1);
 
         String[] input = br.readLine().split(" ");
+
         for(int i = 0; i < N; i++){
             int now = Integer.parseInt(input[i]);
-            
-            
-            while(!st.isEmpty() && st.peek()[0] < now){
-                int idx = st.pop()[1];
-                
-                ans[idx] = now;
+            while(!st.isEmpty() && st.peek()[0] < now) {
+                arr[st.pop()[1]] = now;
             }
 
             st.add(new int[]{now, i});
         }
 
-        for(int a : ans){
-            bw.write(a + " ");
-        }
-        bw.write("\n");
+        for(Integer i : arr) bw.append(i + " ");
+        bw.append("\n");
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
