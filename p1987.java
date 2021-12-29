bdb537990ace325975d2712ffc5128d3068c2671
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-29 16:15:17 +0900

    p1987

--- p1987.java
+++ p1987.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class p1987 {
-    public static int N;
-    public static int M;
+    public static int N, M, res;
+    public static boolean[] visited = new boolean['Z' - 'A' + 1];
     public static int[] dy = {0, 1, 0, -1};
     public static int[] dx = {1, 0, -1, 0};
     public static char[][] map;
-    public static Map<Character, Integer> mp = new HashMap<>();
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+
         N = Integer.parseInt(input[0]);
         M = Integer.parseInt(input[1]);
         map = new char[N][M];
@@ -23,22 +20,24 @@ public class p1987 {
             map[i] = br.readLine().toCharArray();
         }
 
-        System.out.println(dfs(0, 0, 0));
+        dfs(0, 0, 1);
+
+        System.out.println(res);
     }
 
-    public static int dfs(int y, int x, int depth){        
-        if(mp.containsKey(map[y][x])) return depth;
-        mp.put(map[y][x], 1);
-        int ret = 0;
+    public static void dfs(int y, int x, int depth){
+        char c = map[y][x];
+        if(visited[c - 'A']) return;
+        visited[c - 'A'] = true;
+        res = Math.max(res, depth);
+
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
+
             if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-            
-            ret = Math.max(ret, dfs(ny, nx, depth + 1));
+            dfs(ny, nx, depth + 1);
         }
-        
-        mp.remove(map[y][x]);
-        return ret;
+        visited[c - 'A'] = false;
     }
 }
