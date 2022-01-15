91159d44326ae9e3b6f19848c4eafb7959b57bc1
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-15 13:56:28 +0900

    p1062

--- p1062.java
+++ p1062.java
@@ -3,84 +3,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class p1062 {
-    public static int N, M, res;
-    public static String[] input;
+    public static int N, K;
+    public static int[] words;
+    public static BufferedReader br;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        input = br.readLine().split(" ");
+        init();
+        System.out.println(go(0, K, 0));    //index, canLearn, mask;
+    }
+    
+    public static void init() throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
         N = Integer.parseInt(input[0]);
-        M = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[1]);
+        words = new int[N];
 
-        
-        input = new String[N];
         for(int i = 0; i < N; i++){
-            input[i] = br.readLine();
-        }
-        
-        if(M < 5){
-            System.out.println(0);
-            return;
-        } else if(M == 26) {
-            System.out.println(N);
-            return;
-        } else{
-            int bm = 0;
-            bm = turnOnBit(bm, getBitIdx('a'));
-            bm = turnOnBit(bm, getBitIdx('n'));
-            bm = turnOnBit(bm, getBitIdx('t'));
-            bm = turnOnBit(bm, getBitIdx('i'));
-            bm = turnOnBit(bm, getBitIdx('c'));
+            input[0] = br.readLine();
 
-            combi(0, bm, 5);
-
-            System.out.println(res);
+            for(char c : input[0].toCharArray()){
+                words[i] |= (1 << (c - 'a'));
+            }
         }
     }
 
-    public static int getBitIdx(char c){
-        return c - 'a';
-    }
+    public static int go(int idx, int canLearn, int mask){
+        if(canLearn < 0) return 0;
+        if(idx == 26) return count(mask);
+        int ret = go(idx + 1, canLearn - 1, mask | (1 << idx));
 
-    public static int turnOnBit(int num, int idx){
-        return num | (1 << idx);
-    }
-
-    public static int turnOffBit(int num, int idx){
-        return num & ~(1 << idx);
-    }
-
-    public static void combi(int start, int bm, int depth){
-        if(depth == M){
-            res = Math.max(res, getCnt(bm));
-            return;
+        if(anticCheck(idx)){
+            ret = Math.max(ret, go(idx + 1, canLearn, mask));
         }
-
-        for(int i = start; i <= 'z' - 'a'; i++){
-            if((bm & (1 << i))!= 0) continue;
-            bm = turnOnBit(bm, i);
-            combi(i + 1, bm, depth + 1);
-            bm = turnOffBit(bm, i);
-
-        }
-    }
-
-    public static int getCnt(int bm){
-        int ret = 0;
-
-        for(int i = 0; i < input.length; i++){
-            if(canLearn(input[i], bm)) ret++;
-        }
-
         return ret;
     }
 
-    public static boolean canLearn(String word, int bm){
-        
-        for(char c : word.toCharArray()){
-            
-            if(((1 << getBitIdx(c)) & bm) == 0) return false;
+    public static int count(int mask){
+        int ret = 0;
+        for(int wordMask : words){
+            if((mask & wordMask) == wordMask) ret++;
         }
+        return ret;
+    }
 
+    public static boolean anticCheck(int idx){
+        if(idx == 'a' - 'a' || idx == 'n' - 'a' || idx == 't' - 'a' || idx == 'i' - 'a' || idx == 'c' - 'a') return false;
         return true;
     }
 }
