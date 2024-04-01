21620fbc72a4882d4f8f599801413352104b4eec
wood <wood@101.inc>
2024-04-01 17:56:04 +0900

    p12789

---
+++ p12789.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N, curr = 1;
+  cin >> N;
+  int arr[N];
+
+  for(int i = 0; i < N; i++){
+    cin >> arr[i];
+  }
+
+  stack<int> st;
+
+  for(int i = 0; i < N; i++){
+    if(st.empty() || arr[i] < st.top()){
+      st.push(arr[i]);
+    } else if(arr[i] > st.top()){
+      break;
+    }
+
+    while(!st.empty() && curr == st.top()){
+      st.pop();
+      curr++;
+    }
+  }
+
+  if(st.empty()) cout << "Nice" << "\n";
+  else cout << "Sad" << "\n";
+}
