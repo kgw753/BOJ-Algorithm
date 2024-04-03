6aeed46206691725a9bd71d7f17b7dd722465e0b
kgw753 <kgw753@naver.com>
2024-04-03 15:40:27 +0900

    p9996

--- p9996.cpp
+++ p9996.cpp
@@ -1,40 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int N;
+string str, input;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cout.tie(NULL);
+  cin.tie(NULL);
+  cin >> N;
+  cin >> str;
 
-int n;
-string pt;
-vector<string> str;
+  int pos = str.find('*');
+  string prefix = str.substr(0, pos);
+  string suffix = str.substr(pos + 1);
 
-void input(int n){
-    string input;
-    for(int i = 0; i < n; i++){
-        cin >> input;
-        str.push_back(input);
+  for (int i = 0; i < N; i++) {
+    cin >> input;
+
+    if (prefix.size() + suffix.size() > input.size()) {
+      cout << "NE\n";
+    } else {
+      if (prefix == input.substr(0, pos) &&
+          suffix == input.substr(input.size() - suffix.size()))
+        cout << "DA\n";
+      else
+        cout << "NE\n";
     }
-}
-int main(){
-    cin >> n;
-    cin >> pt;
-    input(n);
-    int pos = pt.find('*');
-    string prefix = pt.substr(0, pos);
-    string suffix = pt.substr(pos + 1);
-
-    for(int i = 0; i < n; i++){
-        bool res = false;
-        if(pt.length() - 1 > str[i].length()){
-            cout << "NE\n";
-            continue;
-        }
-
-        if(prefix == str[i].substr(0, pos) &&
-        suffix == str[i].substr(str[i].length() - suffix.length())) res = true;
-
-        if(res){
-            cout << "DA\n";
-        }
-        else{
-            cout << "NE\n";
-        }
-    }
+  }
 }
