7435d326412e82d757c9414e8900c2562961cc69
kgw753 <kgw753@naver.com>
2024-04-05 19:47:37 +0900

    p15686

--- p15686.cpp
+++ p15686.cpp
@@ -1,49 +1,64 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MAX 54
-int N, M, input, ans = 1e9;
-int MAP[MAX][MAX];
-vector<pair<int, int>> chick, hom;
-int getChickDist(int idx, int bm){
-    int ret = 1e9;
-    for(int i = 0; i < chick.size(); i++){
-        if((1 << i) & bm){
-            int tmp = abs(hom[idx].first - chick[i].first) + abs(hom[idx].second - chick[i].second);
-            ret = min(ret, tmp);
-        }
-    }
-    return ret;
+const int MAX = 54;
+int N, M, _map[MAX][MAX], res = 1e9;
+vector<pair<int, int>> chickPos, homePos;
+vector<vector<int>> chickIdxCombi;
+
+void fillChickCombi(int start, vector<int> chickIdxList) {
+  if (chickIdxList.size() == M) {
+    chickIdxCombi.push_back(chickIdxList);
+    return;
+  }
+
+  for (int i = start + 1; i < chickPos.size(); i++) {
+    chickIdxList.push_back(i);
+    fillChickCombi(i, chickIdxList);
+    chickIdxList.pop_back();
+  }
 }
-int getDist(int bm){
-    int dist = 0;
-    for(int i = 0; i < hom.size(); i++){
-        dist += getChickDist(i, bm);
+
+void solve() {
+  fillChickCombi(-1, {});
+
+  for (vector<int> chickIdxList : chickIdxCombi) {
+    int sum = 0;
+    for (pair<int, int> home : homePos) {
+      int minDist = 1e9;
+      int homeY = home.first;
+      int homeX = home.second;
+
+      for (int idx : chickIdxList) {
+        int chickY = chickPos[idx].first;
+        int chickX = chickPos[idx].second;
+        int dist = abs(homeY - chickY) + abs(homeX - chickX);
+        minDist = min(minDist, dist);
+      }
+
+      sum += minDist;
     }
-    return dist;
+    res = min(res, sum);
+  }
+
+  cout << res << "\n";
 }
-bool chickCnt(int bm){
-    int cnt = 0;
-    while(bm > 0){
-        if(bm % 2) cnt++;
-        bm /= 2;
+
+void input() {
+  cin >> N >> M;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> _map[i][j];
+      if (_map[i][j] == 1) {
+        homePos.push_back({i, j});
+      } else if (_map[i][j] == 2) {
+        chickPos.push_back({i, j});
+      }
     }
-    return cnt > M;
+  }
 }
-int main(){
-    cin >> N >> M;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cin >> MAP[i][j];
-            if(MAP[i][j]){
-                if(MAP[i][j] == 1) hom.push_back({i, j});
-                else if(MAP[i][j] == 2) chick.push_back({i, j});
-            }
-        }
-    }
-    for(int i = 1; i < (1 << chick.size()); i++){
-        int tmp = 0;
-        if(chickCnt(i)) continue;
-        ans = min(ans, getDist(i));
-    }
-    cout << ans << "\n";
+
+int main() {
+  input();
+  solve();
 }
